04768a266dc91e57790409e4aeeef359d307d1ef
hzx <huang_zheng_xiang@163.com>
2024-06-05 14:24:25 +0800

    add Windows C compilation supports

---
+++ WindowsDevelopment/C/example/default_matmul_f32.h
@@ -0,0 +1,7 @@
+#ifndef DEFAULT_MATMUL_F32_H_
+#define DEFAULT_MATMUL_F32_H_
+
+
+void default_matmul_f32(float* A, float* B, float* C, int F, int S, int T);
+
+#endif
---
+++ WindowsDevelopment/C/example/main.c
@@ -0,0 +1,9 @@
+#include "default_matmul_f32.h"
+#include <stdio.h>
+
+int main(){
+    // Your code here! Perform a matmul by calling default_matmul_f32.
+    // 
+    printf("Hello Windows World!\n");
+    return 0;
+}
